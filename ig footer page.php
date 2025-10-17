41c73c308a3450f124731cc559e932a13b85a439
PenealSamuel <118816404+PenealSamuel@users.noreply.github.com>
2025-10-17 11:00:18 +0530

    Add files via upload

---
+++ ig footer page.php
@@ -0,0 +1,212 @@
+<div class="wrapper row4">
+
+    <footer id="footer" class="hoc clear">
+
+        <div class="one_quarter first">
+
+            <h6 class="fheading"><i class="fa fa-info-circle"></i>&nbsp;General Information</h6>
+
+            <hr>
+
+
+
+            <nav>
+
+                <ul class="nospace">
+
+                    <li><a href="//igceng.com/aicte.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;AICTE Approvals </a></li><br>
+
+                    <li><a href="//igceng.com/committee/Mandatory-Disclosure-2024-25.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;AICTE Mandatory disclosures</a></li><br>
+
+
+
+                    <!--
+
+<li><a href = "https://aicte-india.org/feedback" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;AICTE Student & Faculty feedback</a></li><br>
+
+<li><a href = "//igceng.com/directorate-of-vigilance-and-anti-corruption-portal.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Vigilance &amp; Anti Corruption</a></li><br>
+
+<li><a href = "//igceng.com/cells/anti-harassment-cell.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Anti-Harassment Cell (General)</a><br />
+
+<li><a href = "//igceng.com/cells/anti-ragging-cell.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Anti Ragging Helpline</a></li><br>
+
+<li><a href = "//igceng.com/cells/anti-sexual-harassment-committee.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Anti-Sexual Harassment Committee </a></li><br>
+
+<li><a href = "//igceng.com/cells/anti-drugs-committee.php"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Anti-Drugs Committee</a></li><br>
+
+-->
+
+
+
+
+
+                    <li><a href="//igceng.com/committee/anti-ragging-committee.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Anti Ragging Committee</a></li><br>
+
+                    <li><a href="//igceng.com/committee/Grievance-Redressal-Commitee.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Grievance Redressal Commitee</a></li><br>
+
+                    <li><a href="//igceng.com/committee/Internal-Complaint-Committee.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Internal Complaint Committee</a></li><br>
+
+                    <li><a href="//igceng.com/committee/Establishment-of-SC-ST-Committee.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp;Establishment of SC ST Committee</a></li><br>
+
+                    <li><a href="//igceng.com/on-line-gri-reg.php"><i class="fa fa-link"></i>&nbsp;On-line Grievances Registration</a></li><br>
+
+                    <li><a href="//igceng.com/on-line-feedback-reg.php"><i class="fa fa-link"></i>&nbsp;On-line Feedback </a></li><br>
+
+                </ul>
+
+                <h1 class="heading1" style="color:#FFFFFF;font-size:10px;font-family:myFont;">" சிறந்த எதிர்காலத்தின் இன்றைய அடித்தளம் "</h1>
+
+            </nav>
+
+
+
+
+
+        </div>
+
+        <div class="one_quarter">
+
+            <h6 class="fheading"><i class="fa fa-handshake-o"></i>&nbsp;Helpful Links</h6>
+
+            <hr>
+
+
+
+            <nav>
+
+                <ul class="nospace">
+
+                    <!--<li><a href = "//igceng.com/right-to-information-act.php/"><i class="fa fa-info-circle"></i>&nbsp;&nbsp;Right To Information Act</a></li><br>-->
+
+                    <li><a href="//igceng.com/psychological-counseling-cell.php" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i>Psychological Counseling Cell</a></li><br>
+
+                    <li><a href="//igceng.com/e-sanad.php" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i> e-Sanad</a></li><br>
+
+                    <li><a href="https://scholarships.gov.in/" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i> National Scholarship </a></li><br>
+
+                    <li><a href="https://swagatam.gov.in/Public/Index.aspx" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i> Gate way to Government</a></li><br>
+
+                    <li><a href="https://www.education.gov.in/" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i> Ministry of Education</a></li><br>
+
+                    <li><a href="//igceng.com/other-links.php"><i class="fa fa-external-link"></i>&nbsp;Other Websites &amp; Links</a></li><br>
+
+                </ul>
+
+
+
+                <h1 class="heading1" style="color:#FFFFFF;font-size:12px;font-family:Roboto;">" Foundation for better Tomorrow "</h1>
+
+            </nav>
+
+        </div>
+
+
+
+
+
+
+
+        <div class="one_quarter">
+
+            <h6 class="fheading"><i class="fa fa-group"></i>&nbsp;Extension Activities</h6>
+
+            <hr>
+
+
+
+            <nav>
+
+                <ul class="nospace">
+
+                    <li><a href="//igceng.com/nss.php" target="_blank"> <i class="fa fa-square" style="font-size:8px;"></i>&nbsp;NSS</a></li>
+
+
+                </ul>
+
+            </nav>
+
+
+
+            <h6 class="fheading"><i class="fa fa-cog"></i>&nbsp;Skill Based Education</h6>
+
+            <hr>
+
+
+
+            <nav>
+
+                <ul class="nospace">
+
+                    <!-- <li><a href="/IGCE/c4i4.php" target="_self">C4i4 Laboratory</a></li> -->
+
+                    <li><a href="//igceng.com/tvse.php" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp; TVS ev Laboratory</a></li>
+
+                    <li><a href="//igceng.com/eduskils.php" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp; Edu-Skills</a></li>
+
+                    <li><a href="//igceng.com/tnea-info-inst.pdf" target="_blank"><i class="fa fa-square" style="font-size:8px;"></i>&nbsp; TNEA Info. & Instructions</a></li>
+
+                </ul>
+
+            </nav>
+
+
+
+            <hr>
+
+            <nav>
+
+                <ul class="nospace">
+
+
+
+
+
+                </ul>
+
+            </nav>
+
+        </div>
+
+
+
+        <div class="one_quarter">
+
+            <h6 class="fheading"><i class="fa fa-map-marker"></i>&nbsp;Visitng IGCE / Contact</h6>
+
+            <hr>
+
+
+
+            <nav>
+
+                <ul class="nospace">
+
+                    <li><a href="//igceng.com/maps-and-directions.php"><i class="fa fa-map-signs"></i>&nbsp;Maps and Directions</a></li><br>
+
+                    <li><a href="//igceng.com/enquiry-contact.php"><i class="fa fa-phone"></i>&nbsp;Enquiry and Contact</a></li><br>
+
+                    <li><a href="//igceng.com/careers.php"><i class="fa fa-square"></i>&nbsp;Careers</a></li><br><br>
+
+                </ul>
+
+            </nav>
+
+        </div>
+
+
+
+    </footer>
+
+    <footer id="footer" class="hoc clear" style="padding: 0 0 0 0;">
+
+        <hr>
+        <span class="elementor-icon-list-icon">
+            <svg aria-hidden="true" class="e-font-icon-svg e-fas-envelope" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path>
+            </svg> </span>
+        <span class="elementor-icon-list-text"><span class="__cf_email__" data-cfemail="b5c6d8c3d0d6f5c6d8c3d0d69bd4d69bdcdb">[email&#160;protected]</span></span>
+        </a>
+        </li>
+        </ul>
+</div>
+</div>
